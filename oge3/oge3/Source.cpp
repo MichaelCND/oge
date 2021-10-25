d88ae326ea38228439a691b6188ca1ff2cf63432
MichaelCND <mffm80580@gmail.com>
2021-10-25 20:30:45 +0300

    1

---
+++ oge3/oge3/Source.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int num;
+int col;
+int res = 0;
+int sum = 0;
+void checkCol();
+
+int main() {
+
+	cout << "vvedite kolichestvo chisel (ne bol'she 1000)";
+	cin >> col;
+
+	if (col <= 1000)
+	{
+		 checkCol();
+	}
+	else
+	{
+		cout << "vy vveli bol'she 1000 chisel";
+	}
+
+	return 0;
+}
+
+void checkCol() {
+	for (int i = 0; i < col; i++)
+	{
+		cout << "vvedite chislo (summa chisel ne dolgzna previshat' 30000)";
+		cin >> num;
+
+		sum = sum + num;
+
+		if (sum <= 30000)
+		{
+			if (num % 100 == 11)
+			{
+				res = res + num;
+			}
+		}
+
+		else
+		{
+			cout << "summa chisel previshaet 30000";
+		}
+
+
+	}
+
+	cout << res;
+}
